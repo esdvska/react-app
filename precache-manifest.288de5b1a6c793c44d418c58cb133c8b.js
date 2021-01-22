4a1fea7b5c0446d4e9ad6cb3b8783be54409b06a
Ewelina Sadowska <esdvska@gmail.com>
2021-01-22 17:21:57 +0100

    Updates

---
+++ precache-manifest.288de5b1a6c793c44d418c58cb133c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d47549497ea5c30eac71",
+    "url": "/react-app/./static/css/main.afd809dd.chunk.css"
+  },
+  {
+    "revision": "d47549497ea5c30eac71",
+    "url": "/react-app/./static/js/main.2970cc86.chunk.js"
+  },
+  {
+    "revision": "e1e6bea1875bc2d725c1",
+    "url": "/react-app/./static/js/runtime~main.1d92ec77.js"
+  },
+  {
+    "revision": "5ab3b37ca35a42ff2d96",
+    "url": "/react-app/./static/js/2.7092e0b8.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c61040d926b138c66ac5ec5c47b1d00c",
+    "url": "/react-app/./index.html"
+  }
+];
